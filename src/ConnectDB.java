315efc8bba18f00a5927767d5944f40007124069
Trung Nguyen Thanh <thanhtrung674@gmail.com>
2015-12-09 14:58:37 +0000

    Test Database connection

---
+++ src/ConnectDB.java
@@ -0,0 +1,27 @@
+import java.sql.*;
+
+public class ConnectDB {
+	private Connection con;
+	public ConnectDB(){
+		con = null;
+		try{
+			String DB ="jdbc:mysql://stusql.dcs.shef.ac.uk/team008?user=team008&password=fff8f017";
+			con = DriverManager.getConnection(DB);
+		}
+		catch(SQLException ex){
+				ex.printStackTrace();
+			}
+			
+	}
+	
+	private Connection getCon(){
+		return con;
+	}
+
+	public static void main(String[] args) {
+		ConnectDB test = new ConnectDB();
+		Connection testDB = test.getCon();
+		System.out.println(testDB);
+	}
+
+}
